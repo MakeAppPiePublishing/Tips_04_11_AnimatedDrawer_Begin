e59d20484f1b0fab5ce7cc9061adee1b1eca38c3
Steve Lipton <steve@makeapppie.com>
2018-09-14 06:54:12 -0500

    Initial Commit of Start State for the Drawer Exercise

--- AnimatedDrawer/ViewController.swift
+++ AnimatedDrawer/ViewController.swift
@@ -2,17 +2,38 @@
 //  ViewController.swift
 //  AnimatedDrawer
 //
-//  Created by Steven Lipton on 9/9/18.
-//  Copyright © 2018 Steven Lipton. All rights reserved.
+//
+//  An exercise file for iOS Development Tips Weekly
+//  by Steven Lipton (C)2018, All rights reserved
+//  For videos go to http://bit.ly/TipsLinkedInLearning
+//  For code go to http://bit.ly/AppPieGithub
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var isOpen = true //The state of the drawer
+    @IBOutlet weak var drawerTrailingConstriant: NSLayoutConstraint!
+    @IBOutlet weak var drawer: UIView!
+    
+    //toggle the state of the drawer
+    @IBAction func openCloseDrawer(_ sender: UIButton) {
+       
+    }
+    
+    // Code the state of the drawer here.
+    func closedDrawer(){
+        
+    }
+    
+    func openedDrawer(){
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //Start with a closed drawer
+        
     }
 
 
